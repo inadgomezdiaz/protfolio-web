1dd22a3baa1c6c7e7d332d5bc539c0fd443cf06e
inadgomezdiaz <cotoop@gmail.com>
2022-10-10 16:28:31 -0300

    actualizacion-1

--- src/componentes/BootstrapSlider.jsx
+++ src/componentes/BootstrapSlider.jsx
@@ -1,15 +1,15 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Proyecto from "./Proyecto"
-import video from "../media/video.mp4"
+import video from "../media/video-tg.mp4"
 
 function BootsrapSlider() {
   return (
     <Carousel slide={false}>
       <Carousel.Item>
         <Proyecto
-          nombre="prueba"
-          demo="youtube.com" github="https//google.com" video={video}
-          descripcion="descripciond video 1" />
+          nombre="Typing Game"
+          demo="https://inadgomezdiaz.github.io/typing-game/" github="https://github.com/inadgomezdiaz/typing-game" video={video}
+          descripcion="juego que mide el tiempo que tardas en copiar una frase (JS,CSS,HTML)" />
       </Carousel.Item>
       <Carousel.Item>
         <Proyecto
